72a17153dbb0cb6f063dd6be36901a8a0d7df9b1
pl2py <matos_wilson@yahoo.es>
2020-06-17 09:59:54 -0400

    Only 5 chances allowed

--- guess.py
+++ guess.py
@@ -8,13 +8,16 @@ from random import randint
 
 def main():
         """
+           guess.py  Ver 1.1
         """
+        chances=5
         print "Guess a number from 0 to 10"
         num=randint(0,10)
-        print 'num=', num
-
-        while True:
+        #print 'num=', num
+        i=1
+        while i<=chances:
                 print
+                print "Time {} of {}".format(i,chances)
                 guess=raw_input("Enter your guess:")
                 try:
                    guess=int(guess)
@@ -28,7 +31,18 @@ def main():
                    break
                 else:
                    print "Keep trying..."     
+                   if guess>num:
+                           print "Too high..."
+                   else:
+                           print "Too low..."
+                   #endif
                 #endif
+                i=i+1   
+                if i>chances:
+                        print "I won!" 
+                        print "My number was:",num
+                        print "Please try again!"
+                #endif        
         #endwhile
 #enddef main()
                    
